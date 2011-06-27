4e257655a04730114f2ee0bd1d02e553ba7aff93
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-06-27 20:53:41 +0100

    Moved stuff around

---
+++ src/test/resources/cucumber/runtime/rhino/stepdefs.js
@@ -0,0 +1,25 @@
+function assertEquals(expected, actual) {
+    if (expected != actual) {
+        throw "Expected " + expected + ", but got " + actual;
+    }
+}
+
+World(function() {
+
+});
+
+Given(/^I have (\d+) cukes in my belly$/, function(n) {
+    this.cukes = n;
+});
+
+Then(/^there are (\d+) cukes in my belly$/, function(n) {
+    assertEquals(n, this.cukes);
+});
+
+Then(/^the (.*) contains (.*)$/, function(container, ingredient) {
+  assertEquals("glass", container)
+});
+
+When(/^I add (.*)$/, function(liquid) {
+  assertEquals("milk", liquid);
+});
